fe57f758218b34bcac648bd0db4e1c8fb00d3242
Aduful Lennox <lennyylennox@gmail.com>
2023-10-25 01:56:22 +0000

    Menu Screen UI Implementation

--- lib/views/user/menu/menu_screen.dart
+++ lib/views/user/menu/menu_screen.dart
@@ -1,3 +1,6 @@
+import 'package:amazon/constants/common_functions.dart';
+import 'package:amazon/utils/colors.dart';
+import 'package:amazon/views/user/home/app_bar.dart';
 import 'package:flutter/material.dart';
 
 class MenuScreen extends StatefulWidget {
@@ -10,10 +13,91 @@ class MenuScreen extends StatefulWidget {
 class _MenuScreenState extends State<MenuScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Menu Screen",
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    final textTheme = Theme.of(context).textTheme;
+    return SafeArea(
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size(width, height * 0.08),
+          child: const HomePageAppBar(),
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            width: width,
+            padding: EdgeInsets.symmetric(
+              horizontal: width * 0.03,
+              vertical: height * 0.02,
+            ),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: appBarGradientColor,
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+              ),
+            ),
+            child: Column(
+              children: [
+                GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10,
+                    childAspectRatio: 0.7,
+                  ),
+                  shrinkWrap: true,
+                  physics: const PageScrollPhysics(),
+                  itemCount: 18,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image:
+                              AssetImage("assets/images/menu_pics/$index.png"),
+                          fit: BoxFit.fill,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: greyShade3),
+                      ),
+                    );
+                  },
+                ),
+                CommonFunctions.blankSpace(height * 0.02, 0),
+                ListView.builder(
+                  itemCount: 2,
+                  shrinkWrap: true,
+                  physics: const PageScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.symmetric(vertical: height * 0.005),
+                      padding: EdgeInsets.symmetric(
+                          vertical: height * 0.005, horizontal: width * 0.02),
+                      width: width,
+                      height: height * 0.06,
+                      decoration: BoxDecoration(
+                        color: white,
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: teal),
+                      ),
+                      child: Row(
+                        children: [
+                          Text(
+                            index == 0 ? "Settings" : "Customer",
+                            style: textTheme.bodyMedium,
+                          ),
+                          const Spacer(),
+                          Icon(
+                            Icons.chevron_right_rounded,
+                            color: black,
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
